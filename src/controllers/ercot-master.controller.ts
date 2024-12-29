93c31ad492e40e702cf69ee2cffe28d08052900e
NikKowPHP <nik.kow@outlook.com>
2024-12-29 08:45:45 +0100

    feat: Enhance controller with validation and proper error responses

--- src/controllers/ercot-master.controller.ts
+++ src/controllers/ercot-master.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Query, 
+  UseInterceptors, 
+  ValidationPipe, 
+  Logger 
+} from '@nestjs/common';
 import { ErcotMasterService } from '../services/ercot-master.service';
+import { AddressSearchDto } from '../dto/address-search.dto';
+import { AddressSuggestion } from '../common/types/ercot.types';
+import { LoggingInterceptor } from '../common/interceptors/logging.interceptor';
 
 @Controller('ercot-master')
+@UseInterceptors(LoggingInterceptor)
 export class ErcotMasterController {
+  private readonly logger = new Logger(ErcotMasterController.name);
+
   constructor(private readonly ercotMasterService: ErcotMasterService) {}
 
+  /**
+   * GET /ercot-master/search
+   * Returns address suggestions based on search term
+   */
   @Get('search')
-  async searchAddresses(@Query('search') searchTerm: string) {
-    return this.ercotMasterService.searchAddresses(searchTerm);
+  public async searchAddresses(
+    @Query(new ValidationPipe({ transform: true })) query: AddressSearchDto,
+  ): Promise<AddressSuggestion[]> {
+    this.logger.log(`Received address search request with term: ${query.search}`);
+    return this.ercotMasterService.searchAddresses(query.search);
   }
 } 
